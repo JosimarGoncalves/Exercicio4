34f2b8c74fd03223870aaeb0c0b9ffc40063d107
Josimar Goncalves <foxchanger@hotmail.com>
2022-04-08 00:26:59 -0300

    Add project files.

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+class Program
+{
+    public const int l = 3;
+    public const int c = 3;
+    public static int[,] mat = new int[l, c];
+    public static int somaA = 0;
+    public static int somaB = 0;
+    public static int somaTotal = 0;
+    static void Main()
+    {
+        Random rnd = new Random();
+
+        var stopWatch = new Stopwatch();
+
+        stopWatch.Start();
+
+        Console.WriteLine("Dimensão da Matriz: " + l + "x" + c + "\n");
+
+        for (int i = 0; i < l; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                mat[i, j] = rnd.Next(1, 10);
+                Console.Write("\t" + mat[i, j]);
+            }
+            Console.WriteLine(" ");
+        }
+
+        Thread t1 = new Thread(SomarA);
+        Thread t2 = new Thread(SomarB);
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        somaTotal = somaA + somaB;
+
+        Console.WriteLine("Soma Total : " + somaTotal);
+       
+
+    }
+    private static void SomarA()
+    {
+        for (int i = 0; i < l/2; i++)
+        {
+            for (int j = 0; j < c/2; j++)
+            {
+                somaA += mat[i, j];
+                //Console.Write(mat[i, j]);
+            }
+            Console.WriteLine(" ");
+        }
+        //Console.WriteLine("\nSoma total: " + soma);
+        
+    }
+
+    private static void SomarB()
+    {
+        for (int i = (l) / 2; i < l; i++)
+        {
+            for (int j = (c) / 2; j < c; j++)
+            {
+                somaB += mat[i, j];
+                //Console.Write("\t" + mat[i, j]);
+            }
+            Console.WriteLine(" ");
+        }
+        //Console.WriteLine("\nSoma total: " + soma);
+
+    }
+
+}
